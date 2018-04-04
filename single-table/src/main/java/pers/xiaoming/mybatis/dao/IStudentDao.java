8ba7441cd6215b971cf7397c9f773ed8b04e1473
Xiaoming Sun <rsun2014@gmail.com>
2018-04-03 21:00:25 -0700

    Define CURD in StudentDAO

--- single-table/src/main/java/pers/xiaoming/mybatis/dao/IStudentDao.java
+++ single-table/src/main/java/pers/xiaoming/mybatis/dao/IStudentDao.java
@@ -2,6 +2,16 @@ package pers.xiaoming.mybatis.dao;
 
 import pers.xiaoming.mybatis.entity.Student;
 
+import java.util.List;
+
 public interface IStudentDao {
-    void insert(Student student);
+    void create(Student student);
+
+    Student get(int id);
+
+    List<Student> getAll();
+
+    void update(Student student);
+
+    void delete(int id);
 }
