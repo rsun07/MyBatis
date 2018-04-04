9953272038bc29daf18922fb132d9ad365abe3c7
Xiaoming Sun <rsun2014@gmail.com>
2018-04-03 21:54:31 -0700

    Initial functionl test environment

--- single-table/src/test/java/InitDB.java
+++ single-table/src/test/java/InitDB.java
@@ -1,2 +1,14 @@
+import org.apache.ibatis.session.SqlSession;
+import org.testng.annotations.BeforeSuite;
+import pers.xiaoming.mybatis.dao.SessionManager;
+
 public class InitDB {
+
+    @BeforeSuite
+    public static void InitDB() {
+        try (SqlSession session = SessionManager.getSession()) {
+            session.delete("truncateStudentTable");
+        }
+    }
+
 }
