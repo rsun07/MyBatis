5686a7e871577bfd47798a1f0bd9c532139093eb
Xiaoming Sun <rsun2014@gmail.com>
2018-04-16 21:11:24 -0700

    initDb for test

--- first-level-cache/src/main/java/pers/xiaoming/mybatis/entity/Student.java
+++ first-level-cache/src/main/java/pers/xiaoming/mybatis/entity/Student.java
@@ -1,14 +1,21 @@
 package pers.xiaoming.mybatis.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Student {
     private int id;
     private String name;
     private double score;
+
+    public Student(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
 }
